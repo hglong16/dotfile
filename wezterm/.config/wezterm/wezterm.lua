20428a8f809c681f904632a27cf507ed44a51f58
hglong16 <intihad.vuong@gmail.com>
2022-10-25 11:17:42 +0700

    minimal wezterm

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require 'wezterm'
+
+return {
+  tab_max_width = 16,
+  text_background_opacity = 0.7,
+  hide_tab_bar_if_only_one_tab = false,
+  font_size = 16,
+  window_background_opacity = 0.75,
+  font = wezterm.font('Fira Code'),
+  color_scheme = "Catppuccin Mocha",
+  tab_bar_at_bottom = true,
+}
