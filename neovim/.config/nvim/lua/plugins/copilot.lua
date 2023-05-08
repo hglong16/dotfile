f74b8e1b08532077d3088d3ab5539f21fb41f46b
hglong16 <intihad.vuong@gmail.com>
2023-05-08 11:09:18 +0700

    disalbe some plugin make LazyVim clean

--- neovim/.config/nvim/lua/plugins/copilot.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "github/copilot.vim",
-  config = function()
-    vim.g.copilot_no_tab_map = true
-    vim.keymap.set("i", "<C-j>", 'copilot#Accept("")', { silent = true, expr = true, desc = "Copilot dummy accept" })
-  end,
-}
---
+++ neovim/.config/nvim/lua/plugins/disalbed.lua
@@ -0,0 +1,6 @@
+return {
+  { "folke/trouble.nvim", enabled = false },
+  { "nvim-pack/nvim-spectre", enabled = false },
+  { "ggandor/flit.nvim", enabled = false },
+  { "ggandor/leap.nvim", enabled = false },
+}
